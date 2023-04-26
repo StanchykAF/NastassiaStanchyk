f3434d50389f388c8463fad8333f354d3f711132
Nastassia Stanchyk <stanchikaf@gmail.com>
2023-04-26 09:17:24 +0300

    Write a functional interface

--- src/main/java/com/epam/javacourse/hometask8/FunctionalInterfacesTask.java
+++ src/main/java/com/epam/javacourse/hometask8/FunctionalInterfacesTask.java
@@ -1,6 +1,7 @@
 package com.epam.javacourse.hometask8;
 
 import java.util.Arrays;
+import java.util.function.BiFunction;
 import java.util.function.BinaryOperator;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -14,9 +15,16 @@ public class FunctionalInterfacesTask {
             Arrays.stream(w.toLowerCase().split(""))
                     .sorted()
                     .collect(Collectors.joining());
+        BiFunction<String, String, Boolean> isAnagram = (w1, w2) ->
+                normalizeWord.apply(w1).equals(normalizeWord.apply(w2));
 
         System.out.println(sumTwoNumbers.apply(3, 6));
         System.out.println("Are " + word1 + " and " + word2 + " anagrams? " +
-                normalizeWord.apply(word1).equals(normalizeWord.apply(word2)));
+                isAnagram.apply(word1, word2));
+
+        MyInteface<Integer> isDividedBy31 = n -> n % 31 == 0;
+
+        System.out.println(isDividedBy31.apply(45));
+        System.out.println(isDividedBy31.apply(62));
     }
 }
